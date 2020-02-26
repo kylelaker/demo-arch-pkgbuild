d1640fef8dbd38fe586eedd340af788655415720
Kyle Laker <kyle@laker.email>
2020-02-26 07:56:59 -0500

    Fix multi-account build

--- aws-build.tf
+++ aws-build.tf
@@ -1,98 +1,121 @@
 provider "aws" {
-    region = "us-east-1"
+  # us-east-1 (Northern Virginia) is the only supported region currently
+  region  = var.aws_region
+  profile = var.r53_profile_name
 }
 
 provider "aws" {
-    alias   = "r53"
-    # us-east-1 (Northern Virginia) is the only supported region currently
-    region  = "us-east-1"
-    profile = var.r53_profile_name
+  alias = "r53"
+  # us-east-1 (Northern Virginia) is the only supported region currently
+  region  = var.aws_region
+  profile = var.r53_profile_name
 }
 
 provider "aws" {
-    alias   = "ec2"
-    # us-east-1 (Northern Virginia) is the only supported region currently
-    region  = "us-east-1"
-    profile = var.ec2_profile_name
+  alias = "ec2"
+  # us-east-1 (Northern Virginia) is the only supported region currently
+  region  = var.aws_region
+  profile = var.ec2_profile_name
 }
 
 data "aws_route53_zone" "domain" {
-    provider = aws.r53
+  provider = aws.r53
 
-    name     = "${var.domain}."
+  name = "${var.domain}."
 }
 
 data "aws_vpc" "default_vpc" {
-    provider = aws.ec2
-    default  = true
+  provider = aws.ec2
+  default  = true
+}
+
+data "aws_ami" "uplink_ami" {
+  provider    = aws.ec2
+  owners      = ["093273469852"] # Uplink Labs
+  most_recent = true
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["arch-linux-hvm-*"]
+  }
 }
 
 resource "aws_key_pair" "arch_ssh" {
-    provider        = aws.ec2
-    key_name_prefix = "archaccesskey"
-    public_key      = file(var.pubkey_file)
+  provider        = aws.ec2
+  key_name_prefix = "archaccesskey"
+  public_key      = file(var.pubkey_file)
 }
 
 resource "aws_security_group" "arch_ssh_access" {
-    provider    = aws.ec2
-    name        = "arch-ssh-access-uug"
-    description = "Allow inbound traffic from home and JMU"
-    vpc_id      = data.aws_vpc.default_vpc.id
+  provider    = aws.ec2
+  name        = "arch-ssh-access-uug"
+  description = "Allow inbound traffic from home and JMU"
+  vpc_id      = data.aws_vpc.default_vpc.id
 
-    # Allow SSH from JMU and home
-    ingress {
-        protocol    = "tcp"
-        from_port   = 22
-        to_port     = 22
-        cidr_blocks = [
-            # Go Dukes!!
-            "134.126.0.0/16",
-            var.my_home_cidr
-        ]
-    }
+  ingress {
+    description = "Allow SSH from JMU and home"
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = [
+      # Go Dukes!!
+      "134.126.0.0/16",
+      var.my_home_cidr
+    ]
+  }
 
-    # Allow ICMP from anywhere
-    ingress {
-        protocol    = "icmp"
-        from_port   = -1
-        to_port     = -1
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "Allow ICMP from anywhere"
+    protocol    = "icmp"
+    from_port   = -1
+    to_port     = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    # Allow egress to anywhere (Terraform removes the default equivalent rule)
-    egress {
-        protocol    = "-1"
-        from_port   = 0
-        to_port     = 0
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    description = "Allow egress anywhere"
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "arch" {
-    provider       = aws.ec2
+  provider = aws.ec2
 
-    ami            = var.ami_id
-    instance_type  = var.instance_type
+  ami           = data.aws_ami.uplink_ami.image_id
+  instance_type = var.instance_type
 
-    vpc_security_group_ids = [aws_security_group.arch_ssh_access.id]
+  vpc_security_group_ids = [aws_security_group.arch_ssh_access.id]
 
-    key_name  = aws_key_pair.arch_ssh.key_name
-    user_data = file(var.userdata_file)
+  key_name  = aws_key_pair.arch_ssh.key_name
+  user_data = file(var.userdata_file)
 
-    tags = {
-        Purpose = "UUG"
-        Name = "UUG-ARCH-${count.index}"
-    }
+  tags = {
+    Purpose = "UUG"
+    Name    = format("UUG-ARCH-%02d", count.index + 1)
+  }
 
-    count = var.attendee_count
+  count = var.attendee_count
 }
 
 resource "aws_route53_record" "arch-r53" {
-    provider = aws.r53
-    count    = var.attendee_count
-    zone_id  = data.aws_route53_zone.domain.zone_id
-    name     = "arch${count.index}.uug"
-    type     = "A"
-    ttl      = "60"
-    records  = ["${element(aws_instance.arch.*.public_ip, count.index)}"]
+  provider = aws.r53
+  count    = var.attendee_count
+  zone_id  = data.aws_route53_zone.domain.zone_id
+  name     = format("arch%02d.uug", count.index + 1)
+  type     = "A"
+  ttl      = "60"
+  records  = ["${element(aws_instance.arch.*.public_ip, count.index)}"]
 }
--- variables.tf
+++ variables.tf
@@ -1,36 +1,40 @@
 variable "ami_id" {
-    default = "ami-074508ad6e83609f9"
+  default = "ami-074508ad6e83609f9"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
 }
 
 variable "r53_profile_name" {
-    type = string
+  type = string
 }
 
 variable "ec2_profile_name" {
-    type = string
+  type = string
 }
 
 variable "my_home_cidr" {
-    type = string
+  type = string
 }
 
 variable "attendee_count" {
-    type = number
+  type = number
 }
 
 variable "domain" {
-    type = string
+  type = string
 }
 
 variable "pubkey_file" {
-    type = string
+  type = string
 }
 
 variable "userdata_file" {
-    type = string
+  type = string
 }
 
 variable "instance_type" {
-    type = string
-    default = "t3.micro"
+  type    = string
+  default = "t3.micro"
 }
